c4eaddddbc223909fb695bf986368adaf39818f1
CarlosPepin <carlos.pepin.peralta@gmail.com>
2020-12-13 21:57:37 -0400

    Second commit, first test passed, refactored

--- Range.cs
+++ Range.cs
@@ -4,7 +4,25 @@ namespace UnitTestRange
 {
     public class Range
     {
-        public int Left=1, Right=2;
-        //public void         
+        public int Left, Right;
+        public bool IsLeftOpen, IsRightOpen;
+        public string rango;
+
+        public Range(string rango){
+            this.rango = rango;
+            this.IsLeftOpen = rango.Contains("(");
+            this.IsRightOpen = rango.Contains(")");
+
+            string[] separado = rango.Split(',');
+            int.TryParse(separado[0].Substring(1), out this.Left);
+            int.TryParse(separado[1].Substring(0,separado[1].Length-1), out this.Right);
+
+            if (IsLeftOpen)
+                this.Left = Left + 1;
+            if (IsLeftOpen)
+                this.Right = Right + 1;
+
+           
+        }      
     }
 }
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -8,10 +8,12 @@ namespace UnitTestRange
         [Fact]
         public void TestEsc111()
         {
-            Range R = new Range();
+            Range R = new Range("[1,2]");
             var c1 = R.Left == 1;
             var c2 = R.Right == 2;
             Assert.Equal( c1 && c2 ,true);
         }
+
+
     }
 }
